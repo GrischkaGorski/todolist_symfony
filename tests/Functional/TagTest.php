30131103109df7e4f14c8550ac0a4a1871ca14c1
Paul Hoarau <phoarau@consoneo.com>
2023-06-06 18:06:17 +0200

    ✅ First functional tests for tags

--- tests/Functional/TagTest.php
+++ tests/Functional/TagTest.php
@@ -1,23 +1,36 @@
 <?php
-//
-//namespace App\Tests\Functional;
-//
-//use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
-//use App\Entity\Tag;
-//use App\Entity\Todo;
-//use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
-//use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
-//
-//class TagTest extends ApiTestCase
-//{
-//    use RefreshDatabaseTrait;
-//
-//    /**
-//     * @throws TransportExceptionInterface
-//     */
-//    public function testGetCollection(): void {
-//        $response = static::createClient()->request('GET', '/tags');
-//
-//        $this->assertResponseIsSuccessful();
-//    }
-//}
+
+namespace App\Tests\Functional;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+use App\Entity\Tag;
+use App\Kernel;
+use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+
+class TagTest extends ApiTestCase
+{
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
+    }
+
+    use RefreshDatabaseTrait;
+
+    public function testGetCollection(): void
+    {
+        $response = static::createClient()->request('GET', '/api/tags');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Tag',
+            '@id' => '/api/tags',
+            '@type' => 'hydra:Collection',
+            'hydra:totalItems' => 15
+        ]);
+
+        $this->assertCount(15, $response->toArray()['hydra:member']);
+        $this->assertMatchesResourceCollectionJsonSchema(Tag::class);
+    }
+}
