d930aa3ed353ba93631b4925168fbd71903cb796
Paul Hoarau <phoarau@consoneo.com>
2023-06-06 16:41:41 +0200

    🚧 Functional tests inc

---
+++ tests/Functional/TagTest.php
@@ -0,0 +1,23 @@
+<?php
+//
+//namespace App\Tests\Functional;
+//
+//use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+//use App\Entity\Tag;
+//use App\Entity\Todo;
+//use Hautelook\AliceBundle\PhpUnit\RefreshDatabaseTrait;
+//use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+//
+//class TagTest extends ApiTestCase
+//{
+//    use RefreshDatabaseTrait;
+//
+//    /**
+//     * @throws TransportExceptionInterface
+//     */
+//    public function testGetCollection(): void {
+//        $response = static::createClient()->request('GET', '/tags');
+//
+//        $this->assertResponseIsSuccessful();
+//    }
+//}
