b549f147732a02fa077b3cf54814c8c90693e6da
Paul Hoarau <phoarau@consoneo.com>
2023-06-07 11:54:50 +0200

    ✅ Functional tests for tags

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,39 +2,10 @@
 
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
-use App\Service\ActionGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private $manager;
 
-    private $action;
-
-    public function __construct(EntityManagerInterface $manager, ActionGenerator $action)
-    {
-        $this->manager = $manager;
-
-        $this->action = $action;
-    }
-
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        $array_day = $this->action->getAction();
-        $dayDate = $this->action->getAction();
-        $hour = $this->action->getAction();
-
-        return $this->render('pages/Home.html.twig',
-            [
-                '$array_day' => $array_day,
-                '$dayDate' => $dayDate,
-                '$hour' => $hour,
-            ]
-        );
-    }
 }
 
--- tests/Functional/TagTest.php
+++ tests/Functional/TagTest.php
@@ -31,6 +31,61 @@ class TagTest extends ApiTestCase
         ]);
 
         $this->assertCount(15, $response->toArray()['hydra:member']);
-        $this->assertMatchesResourceCollectionJsonSchema(Tag::class);
+    }
+
+    public function testCreateTag(): void {
+        $response = static::createClient()->request('POST', '/api/tags', ['json' => [
+            'name' => 'TestCreateTag Name'
+        ]]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Tag',
+            '@type' => 'Tag',
+            'name' => 'TestCreateTag Name',
+            'todos' => [],
+        ]);
+        $this->assertMatchesRegularExpression('~^/api/tags/\d+$~', $response->toArray()['@id']);
+        $this->assertMatchesResourceItemJsonSchema(Tag::class);
+    }
+
+    public function testCreateInvalidTag(): void
+    {
+        $response = static::createClient()->request('POST', '/api/tags', ['json' => [
+            'name' => 1
+        ]]);
+        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+        $this->assertJsonContains([
+            '@context' => '/api/contexts/Error',
+            '@type' => 'hydra:Error',
+            'hydra:title' => 'An error occurred',
+            'hydra:description' => 'The type of the "name" attribute must be "string", "integer" given.',
+        ]);
+    }
+
+    public function testUpdateTag(): void {
+        $client = static::createClient();
+        $iri = $this->findIriBy(Tag::class, ['name' => 'I should.']);
+
+        $client->request("PUT", $iri, ['json' => [
+            'name' => "TestUpdateTag new name"
+        ]]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            '@id' => $iri,
+            'name' => 'TestUpdateTag new name',
+        ]);
+    }
+
+    public function testDeleteTag(): void {
+        $client = static::createClient();
+        $iri = $this->findIriBy(Tag::class, ['name' => 'I should.']);
+
+        $client->request('DELETE', $iri);
+
+        $this->assertResponseStatusCodeSame(204);
     }
 }
