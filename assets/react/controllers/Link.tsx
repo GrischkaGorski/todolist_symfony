8cd5c46a435b1964377239b4e2e0fcd36480d9f9
Paul Hoarau <phoarau@consoneo.com>
2023-06-13 12:49:05 +0200

    ✨ React component Link

---
+++ assets/react/controllers/Link.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface Props {
+  text: string
+}
+
+export default function (props: Props) {
+    const { text } = props;
+    return (
+        <a
+          href="/todos/create"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-5 py-2.5 focus:outline-none">
+          {text}
+        </a>
+    )
+}
