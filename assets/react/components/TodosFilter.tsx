b2ddb671a8ce6d88f38f672c0999e12bc9323eda
Paul Hoarau <phoarau@consoneo.com>
2023-06-16 10:52:43 +0200

    ✨ Filters feature ok

--- assets/react/components/TodosFilter.tsx
+++ assets/react/components/TodosFilter.tsx
@@ -1,16 +1,14 @@
-import React, {Dispatch, FormEvent, SetStateAction, useEffect, useState} from 'react';
-import {Tag, Todo} from "../controllers/TodoList";
-import Select from "react-select";
+import React, {FormEvent, useEffect, useState} from 'react';
+import {Tag} from "../controllers/TodoList";
 
 type TodosFilterProps = {
-  todos: Todo[]
-  filteredTodos: Todo[]
-  setFilteredTodos: Dispatch<SetStateAction<Todo[]>>
+  handleFilterChange: (filter : { checkedTags: string[], doneFilter: string }) => void
 }
 
-export default function TodosFilter({todos, filteredTodos, setFilteredTodos}: TodosFilterProps) {
+export default function TodosFilter({handleFilterChange}: TodosFilterProps) {
   const [tags, setTags] = useState<Tag[]>([]);
   const [checkedTags, setCheckTags] = useState<string[]>([])
+  const [doneFilter, setDoneFilter] = useState<string>('')
 
   const fetchTags = async (): Promise<void> => {
     try {
@@ -29,14 +27,7 @@ export default function TodosFilter({todos, filteredTodos, setFilteredTodos}: To
 
   const handleDoneFilter = (event: FormEvent<HTMLDivElement>) => {
     const value = (event.target as HTMLInputElement).value;
-
-    if ( value === 'true' ) {
-      setFilteredTodos(todos.filter(todo => todo.done))
-    } else if ( value === 'false') {
-      setFilteredTodos(todos.filter(todo => !todo.done))
-    } else {
-      setFilteredTodos(todos)
-    }
+    setDoneFilter(value);
   }
 
   const handleTagCheck = (e: FormEvent<HTMLInputElement>) => {
@@ -49,34 +40,25 @@ export default function TodosFilter({todos, filteredTodos, setFilteredTodos}: To
   }
 
   useEffect(() => {
-      if (checkedTags.length > 0) {
-        const filteredTodos = todos.filter(todo =>
-          checkedTags.every(checkedTag =>
-            todo.tags.some(tag => tag.id.toString() === checkedTag)
-          )
-        );
-        setFilteredTodos(filteredTodos);
-      } else {
-        setFilteredTodos(todos);
-      }
-    }, [checkedTags]
+      handleFilterChange({checkedTags, doneFilter})
+    }, [checkedTags, doneFilter]
   )
 
   return (
     <div className="flex items-center pl-5">
       <h2 className="h2 text-slate-600">Filtres :</h2>
       <div onChange={(event) => handleDoneFilter(event)} className="flex items-center gap-5 px-5">
-        <div className="flex flex-col items-center justify-between" >
+        <div className="flex flex-col items-center gap-1" >
           <label htmlFor="done-filter" className="text-sm font-medium text-slate-600">Faits</label>
           <input id="done-filter" type="radio" value="true" name="done"
                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-1">
           <label htmlFor="not-done-filter" className="text-sm font-medium text-slate-600">À faire</label>
           <input id="not-done-filter" type="radio" value="false" name="done"
                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
         </div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center gap-1">
           <label htmlFor="not-filtered" className="text-sm font-medium text-slate-600">Tous</label>
           <input defaultChecked={true} id="not-filtered" type="radio" value='' name="done"
                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
@@ -91,7 +73,7 @@ export default function TodosFilter({todos, filteredTodos, setFilteredTodos}: To
           </svg>
         </button>
         <div id="dropdown" className="z-10 hidden bg-white rounded-lg shadow w-fit p-5 flex flex-col gap-2">
-          {tags.map((tag, index )=> (
+          {tags.map((tag )=> (
             <div key={tag.id} className="flex items-center" onChange={handleTagCheck}>
               <input id={`tag-${tag.id}`} type="checkbox" value={tag.id} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
               <label htmlFor={`tag-${tag.id}`} className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
--- assets/react/controllers/TodoList.tsx
+++ assets/react/controllers/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Link from "./Link";
 import TodoItem from "../components/TodoItem"
 import TodosFilter from "../components/TodosFilter";
@@ -25,7 +25,6 @@ export default function TodoList() {
       const response = await fetch("https://localhost/api/todos");
       const data = await response.json();
       setTodos(data["hydra:member"]);
-      setFilteredTodos(todos)
     } catch (error) {
       console.error(error);
     }
@@ -36,14 +35,35 @@ export default function TodoList() {
   }, []);
 
   useEffect(() => {
-    setFilteredTodos(todos);
+    setFilteredTodos(todos)
+  }, [todos])
+
+  const handleFilterChange = useCallback((filter: { checkedTags: string[], doneFilter: string }) => {
+    const { checkedTags, doneFilter } = filter;
+
+    const filteredTodos = todos.filter(todo =>
+      checkedTags.every(checkedTag =>
+        todo.tags.some(tag => tag.id.toString() === checkedTag)
+      )
+    ).filter(todo => {
+      if (doneFilter === 'true') {
+        return todo.done;
+      } else if (doneFilter === 'false') {
+        return !todo.done;
+      } else {
+        return true;
+      }
+    });
+
+    setFilteredTodos(filteredTodos);
+
   }, [todos]);
 
   return (
     <div className="flex flex-col gap-5">
       <div className="flex justify-between">
         <Link text="Nouveau" href="/todos/create" color="blue"/>
-        <TodosFilter todos={todos}  filteredTodos={filteredTodos} setFilteredTodos={setFilteredTodos} />
+        <TodosFilter handleFilterChange={handleFilterChange}/>
       </div>
       {filteredTodos?.length > 0 && (
         <ul className="flex flex-col gap-5">
