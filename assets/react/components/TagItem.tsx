6d8beafafeb737d8d302af74a4dbbd67d790e9a8
Paul Hoarau <phoarau@consoneo.com>
2023-06-27 14:44:14 +0200

    ✨ TagList, delete tags and edit tags

--- assets/react/components/TagItem.tsx
+++ assets/react/components/TagItem.tsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {Tag} from "../controllers/TodoList";
 import {RxCross2} from "react-icons/rx";
-import {AiFillEdit} from "react-icons/ai";
+import {useDebounce} from "../hooks/useDebounce";
 
 interface TodoItemProps {
   tag: Tag;
+  saveNewTag: (newTagName: string, tagId: number) => Promise<void>;
+  deleteTag: (tagId: number) => Promise<void>;
 }
 
-export default function TagItem({ tag }: TodoItemProps) {
+export default function TagItem({ tag, saveNewTag, deleteTag}: TodoItemProps) {
+  const [value, setValue] = useState<string>(tag.name)
+  const debounceValue = useDebounce<string>(value, 500)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  useEffect(() => {
+    if ( debounceValue === tag.name ) {
+      return
+    }
+    saveNewTag(debounceValue, tag.id)
+  }, [debounceValue])
+
   return (
     <li
       className="rounded-md border border-slate-200 shadow shadow-slate-200 hover:bg-blue-50">
-      <div className="flex flex-col text-center gap-4 p-4">
-        <span className="text-md font-bold uppercase px-2 py-1 text-blue-700">
-           {tag.name}
-        </span>
-        <div className="text-slate-600 font-bold flex gap-4 px-4">
-          Tâches :
-          <span>{tag.todos.length}</span>
+      <div className="flex flex-col text-center gap-2 p-4">
+        <div className="flex justify-between items-center gap-8">
+          <input
+            className="text-md font-bold text-blue-500 border-0 focus:outline-none bg-transparent"
+            value={value}
+            onChange={handleChange}
+          />
+          <button onClick={() => deleteTag(tag.id)}>
+            <RxCross2 className="text-red-600 hover:text-red-900 hover:cursor-pointer"/>
+          </button>
         </div>
-        <div className="flex px-4 justify-between">
-          <AiFillEdit className="h-6 w-6 text-yellow-300 hover:text-yellow-500 hover:cursor-pointer"/>
-          <RxCross2 onClick={} className="h-6 w-6 text-red-600 hover:text-red-900 hover:cursor-pointer"/>
-        </div>
+        <p className="text-slate-400 font-bold flex flex-start">
+          Tâches : {tag.todos.length}
+        </p>
       </div>
     </li>
   )
--- assets/react/controllers/TagList.tsx
+++ assets/react/controllers/TagList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {Tag} from "./TodoList";
 import TagItem from "../components/TagItem";
+import Link from "./Link";
 
 export default function TagList(): JSX.Element {
   const [tags,setTags] = useState<Tag[]>([]);
@@ -15,21 +16,52 @@ export default function TagList(): JSX.Element {
     }
   };
 
+  const saveNewTag = async (newTagName: string, tagId: number): Promise<void> => {
+    try {
+      const res = await fetch(`/api/tags/${tagId}`, {
+        method: 'PATCH',
+        headers: {
+          'Accept': 'application/ld+json',
+          'Content-Type': 'application/merge-patch+json'
+        },
+        body: JSON.stringify({ name: newTagName })
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteTag = async (tagId: number): Promise<void> => {
+    try {
+      const res = await fetch(`/api/tags/${tagId}`, {
+        method: 'DELETE',
+      });
+      const updatedTags = tags.filter(tag => tag.id !== tagId);
+
+      setTags(updatedTags);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     fetchTags();
   }, []);
 
-
-  console.log(tags)
   return (
-    <div className="flex flex-col gap-5">
-      {tags?.length > 0 && (
-        <ul className="flex gap-5 flex-wrap">
-          {tags.map(tag => (
-            <TagItem key={tag.id} tag={tag}/>
-          ))}
-        </ul>
-      )}
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-5">
+        {tags?.length > 0 && (
+          <ul className="flex gap-5 flex-wrap">
+            {tags.map(tag => (
+              <TagItem key={tag.id} tag={tag} saveNewTag={saveNewTag} deleteTag={deleteTag}/>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div>
+        <Link color="blue" href="/" text="Accueil"/>
+      </div>
     </div>
   );
 }
