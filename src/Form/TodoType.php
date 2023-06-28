de08fb22aff658cd1c316e4788ffa95083d8dee3
Paul Hoarau <phoarau@consoneo.com>
2023-06-28 15:14:45 +0200

    ✨ Create new tag when creating or editing todo

--- src/Form/TodoType.php
+++ src/Form/TodoType.php
@@ -10,6 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TodoType extends AbstractType
@@ -21,8 +23,22 @@ class TodoType extends AbstractType
             ->add('description', TextType::class, ['label'=> 'Description','required' => false])
             ->add('done', CheckboxType::class, ['label'=> 'Terminée','required' => false])
             ->add('tags', EntityType::class, ['label'=> 'Tags', 'class' => Tag::class, 'choice_label' => 'name', 'multiple' => true, 'required' => false, 'by_reference' => false])
+            ->add('newTag', TextType::class, ['label' => 'Nouveau Tag', 'mapped' => false, 'required' => false])
             ->add('save', SubmitType::class, ['label' => 'Valider'])
         ;
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) {
+            $form = $event->getForm();
+            $todo = $event->getData();
+
+            $newTag = $form->get('newTag')->getData();
+            if ($newTag !== null && $newTag !== '') {
+                $tag = new Tag();
+                $tag->setName($newTag);
+
+                $todo->addTag($tag);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
